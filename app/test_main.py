3b8f33b4ddbf50307eed3c9fb3eec22f278c16d1
Valerii <valeraivasik602@gmail.com>
2026-02-24 23:20:20 +0000

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -6,9 +6,21 @@ from app.main import outdated_products
 
 def test_outdated_products_some_outdated() -> None:
     products = [
-        {"name": "salmon", "expiration_date": datetime.date(2022, 2, 10), "price": 600},
-        {"name": "chicken", "expiration_date": datetime.date(2022, 2, 5), "price": 120},
-        {"name": "duck", "expiration_date": datetime.date(2022, 2, 1), "price": 160},
+        {
+            "name": "salmon",
+            "expiration_date": datetime.date(2022, 2, 10),
+            "price": 600,
+        },
+        {
+            "name": "chicken",
+            "expiration_date": datetime.date(2022, 2, 5),
+            "price": 120,
+        },
+        {
+            "name": "duck",
+            "expiration_date": datetime.date(2022, 2, 1),
+            "price": 160,
+        },
     ]
 
     with patch("app.main.datetime") as mock_datetime:
@@ -21,8 +33,16 @@ def test_outdated_products_some_outdated() -> None:
 
 def test_outdated_products_none_outdated() -> None:
     products = [
-        {"name": "salmon", "expiration_date": datetime.date(2022, 2, 10), "price": 600},
-        {"name": "chicken", "expiration_date": datetime.date(2022, 2, 5), "price": 120},
+        {
+            "name": "salmon",
+            "expiration_date": datetime.date(2022, 2, 10),
+            "price": 600,
+        },
+        {
+            "name": "chicken",
+            "expiration_date": datetime.date(2022, 2, 5),
+            "price": 120,
+        },
     ]
 
     with patch("app.main.datetime") as mock_datetime:
@@ -35,8 +55,16 @@ def test_outdated_products_none_outdated() -> None:
 
 def test_outdated_products_all_outdated() -> None:
     products = [
-        {"name": "salmon", "expiration_date": datetime.date(2022, 1, 10), "price": 600},
-        {"name": "chicken", "expiration_date": datetime.date(2022, 1, 5), "price": 120},
+        {
+            "name": "salmon",
+            "expiration_date": datetime.date(2022, 1, 10),
+            "price": 600,
+        },
+        {
+            "name": "chicken",
+            "expiration_date": datetime.date(2022, 1, 5),
+            "price": 120,
+        },
     ]
 
     with patch("app.main.datetime") as mock_datetime:
